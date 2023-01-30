4af82ff7e61918954100f16a8a0c5b129d40133d
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-30 09:20:40 -0500

    followed along with the guided video to practice using Redux

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -1,38 +1,27 @@
 import React from 'react';
 import DragonMember from './DragonMember';
+import { addMember, updateNewMember } from '../actions/dragonListActions';
+import { connect } from 'react-redux';
 
 class DragonList extends React.Component {
-  state = {
-    newMember: '',
-    members: [
-      { name: 'Jojo Zhang', dragonStatus: true },
-      { name: 'Brandon Harris', dragonStatus: false }
-    ]
-  };
 
-  handleChanges = e => {
-    this.setState({ ...this.state, newMember: e.target.value });
-  };
-
-  handleClick = ()=> {
-    this.setState({
-      ...this.state,
-      members: [...this.members, {name: newMember, dragonStatus: true}]
-    })
+  handleClick = () => {
+    const newMember = { name: this.props.newMember, dragonStatus: true }
+    this.props.addMember(newMember);
   }
 
-  render() {
+   render() {
     return (
       <div>
         <div className="friends-list">
-          {this.state.members.map((member, index) => (
+          {this.props.members.map((member, index) => (
             <DragonMember key={index} member={member}/>
           ))}
         </div>
         <input
           type="text"
-          value={this.state.newMember}
-          onChange={this.handleChanges}
+          value={this.props.newMember}
+          onChange={(e) => this.props.updateNewMember(e.target.value)}
           placeholder="Add new member"
         />
         <button onClick={this.handleClick}>Add member</button>
@@ -41,4 +30,11 @@ class DragonList extends React.Component {
   }
 }
 
-export default DragonList;
+const mapStateToProps = state => {
+  return {
+    members: state.DragonList.members,
+    newMember: state.DragonList.newMember
+  }
+}
+
+export default connect(mapStateToProps, {updateNewMember, addMember})(DragonList);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
 
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_());
+
 function App() {
   return (
     <div className="App">
@@ -15,4 +21,8 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
